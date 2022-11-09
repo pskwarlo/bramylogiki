597ea9053542b4e1a751d6e58fe2dfadc166b66b
Pawel Skwarlo <s184513@student.pg.edu.pl>
2022-11-09 20:36:20 +0100

    Delete target and .idea folders

--- src/main/java/org/example/gates/Gate.java
+++ src/main/java/org/example/gates/Gate.java
@@ -1,5 +1,11 @@
 package org.example.gates;
 
-public class Gate extends com{
+import org.example.modules.Component;
 
+public class Gate extends Component {
+    public Gate(boolean inputAValue, final boolean inputBValue, final boolean output, final int gateTypeId) {
+        super(inputAValue, output);
+        this.inputBValue = inputBValue;
+        this.gateTypeId = gateTypeId;
+    }
 }
