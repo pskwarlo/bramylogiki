ccbee8565a896183b638d610549345e524cac1f6
Pawel Skwarlo <s184513@student.pg.edu.pl>
2022-12-11 23:19:23 +0100

    Dodanie plikow graficznych i fxml

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,15 +15,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        stage.setTitle("Bramy Logiki");
-//        stage.show();
-//        var label2 = new Label("Zaczbuh grę");
-//        var label3 = new Label("Tabela Wyników");
-//        var label4= new Label("Zamknij gre");
-//        var scene = new Scene(new StackPane(label2, label3, label4), 1280, 1024);
-//        stage.setScene(scene);
-//        stage.show();
-        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("AndGate.fxml"));
+        stage.setTitle("Bramy Logiki");
+        //FXMLLoader fxmlloaderAnd = new FXMLLoader(getClass().getResource("AndGate.fxml"));
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("NandGate.fxml"));
         Scene scene = new Scene(fxmlloader.load(), 1280, 1024);
         stage.setScene(scene);
         stage.show();
